cd5c98051efbebfa56ffd553cef230b419d41197
YesNoMaybeItDepends <22201184+YesNoMaybeItDepends@users.noreply.github.com>
2022-08-26 19:06:19 +0200

    Add project files.

---
+++ Server/Controllers/MovieController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieJournal.Shared;
+
+namespace MovieJournal.Server.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class MovieController : ControllerBase
+  {
+    private readonly ILogger<MovieController> _logger;
+    public MovieController(ILogger<MovieController> logger)
+    {
+      _logger = logger;
+    }
+
+    public List<Movie> Movies = new List<Movie>
+    {
+      new Movie
+      {
+        Id = 0,
+        Title = "Street Kings",
+        Year = 2008,
+        Genre = "Crime",
+        Status = "Not Seen",
+        Stars = 55,
+        Review = "Yep"
+      },
+      new Movie
+      {
+        Id = 1,
+        Title = "Spirited Away",
+        Year = 2001,
+        Genre = "Animation",
+        Status = "Seen",
+        Stars = 96,
+        Review = "Yep"
+      }
+    };
+
+    [HttpGet]
+    public async Task<ActionResult<List<Movie>>> GetMovies()
+    {
+      return Ok(Movies);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Movie>> GetSingleMovie(int id)
+    {
+      var movie = Movies.FirstOrDefault(x => x.Id == id);
+      if (movie != null)
+      {
+        return Ok(movie);
+      }
+      return NotFound($"Sorry, no movie found with the id '{id}'");
+    }
+  }
+}
---
+++ Shared/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieJournal.Shared
+{
+  public class Movie
+  {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int? Year { get; set; } = null;
+    public string Genre { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public int? Stars { get; set; } = null;
+    public string Review { get; set; } = string.Empty;
+  }
+}
