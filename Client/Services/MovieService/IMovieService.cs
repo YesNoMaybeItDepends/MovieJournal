f496d9f1a20911426cab87eccd3e83ce2d9d15f2
YesNoMaybeItDepends <22201184+YesNoMaybeItDepends@users.noreply.github.com>
2022-08-26 19:57:12 +0200

    Added MovieService

---
+++ Client/Services/MovieService/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿using MovieJournal.Shared;
+
+namespace MovieJournal.Client.Services.MovieService
+{
+  public interface IMovieService
+  {
+    List<Movie> Movies { get; set; }
+    Task GetMovies();
+    Task<Movie> GetSingleMovie(int id);
+  }
+}
---
+++ Client/Services/MovieService/MovieService.cs
@@ -0,0 +1,35 @@
+﻿using MovieJournal.Shared;
+using System.Net.Http.Json;
+
+namespace MovieJournal.Client.Services.MovieService
+{
+  public class MovieService : IMovieService
+  {
+    private readonly HttpClient _http;
+    public List<Movie> Movies { get; set; } = new List<Movie>();
+    
+    public MovieService(HttpClient http)
+    {
+      _http = http;
+    }
+
+    public async Task GetMovies()
+    {
+      var result = await _http.GetFromJsonAsync<List<Movie>>("api/movie");
+      if (result != null)
+      {
+        Movies = result;
+      }
+    }
+
+    public async Task<Movie> GetSingleMovie(int id)
+    {
+      var result = await _http.GetFromJsonAsync<Movie>($"api/movie/{id}");
+      if (result != null)
+      {
+        return result;
+      }
+      throw new Exception("Movie not found");
+    }
+  }
+}
